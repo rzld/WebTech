2cfe2a14449eddc5532243ed7da199ddd94ff4fe
Rizaldi Tri Yanuar <rzldty@gmail.com>
2017-05-20 21:46:52 +0100

    tried to att image slideshow, but still not working

--- site/test.js
+++ site/test.js
@@ -1,8 +1,29 @@
-"use strict";
-var jsdom = require("jsdom");
-var html = "<!DOCTYPE html><html>Hello world!</html>";
-jsdom.env(html, go);
+/* Slideshow animation */
+/* ref: https://www.w3schools.com/howto/howto_js_slideshow.asp */
 
-function go(err, window) {
-    console.log("Check: ", window.document.querySelector("html"));
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i=0; i<slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i=0; i<dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
